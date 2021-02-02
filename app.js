e7cf3433d7350d0b7d2ddaaeecbcd25f3f1d6430
Coderzzz <hashirk966@gmail.com>
2021-02-02 17:24:15 +0500

    Stop Watch

--- app.js
+++ app.js
@@ -5,18 +5,34 @@ var minHead = document.querySelector(".min");
 var secHead = document.querySelector(".sec");
 var msecHead = document.querySelector(".msec");
 var start = document.querySelector(".start");
+var set = document.querySelector(".set");
+var minsInput = document.querySelector("#mins");
+var table_set_time = document.querySelector(".table_set_time");
+var watch = document.querySelector(".watch");
 var interval;
+
+function setTime() {
+  min = minsInput.value;
+  sec = 59;
+  table_set_time.classList.add("none");
+  watch.classList.remove("none");
+  minHead.innerHTML = min;
+  secHead.innerHTML = sec;
+}
+
 function timer() {
   msec++;
   msecHead.innerHTML = msec;
   if (msec >= 100) {
-    sec++;
+    sec--;
     secHead.innerHTML = sec;
     msec = 0;
-  } else if (sec >= 60) {
-    min++;
+  } else if (sec <= 0) {
+    min--;
     minHead.innerHTML = min;
-    sec = 0;
+    sec = 59;
+  } else if (min <= 0) {
+    clearInterval(interval);
   }
 }
 
